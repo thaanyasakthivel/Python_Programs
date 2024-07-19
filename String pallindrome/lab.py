3619199e289f0a74b8c074e794e660ad104847ba
thaanyasakthivel <skthaanya@gmail.com>
2024-07-19 12:36:45 +0530

    Update lab.py

--- String pallindrome/lab.py
+++ String pallindrome/lab.py
@@ -1,12 +1,4 @@
 def is_palindrome(s):
-    """
-    Instead of returning None, this method should Check if a string is a palindrome
-    and return True or False accordingly.
-    
-    :param s: The string to check.
-    :return: True if the string is a palindrome, False otherwise.
-    """
-    # Convert the string to lowercase and remove non-alphanumeric characters
-    s = ''.join(char.lower() for char in s if char.isalnum())
-    # Compare the string with its reverse
+    cleaned_s = ''.join(char.lower() for char in s if char.isalnum())
+    return cleaned_s == cleaned_s[::-1]
     return None
