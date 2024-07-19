a8469e2095d4f9b04a8422c93bc7e07dfa0238a2
thaanyasakthivel <skthaanya@gmail.com>
2024-07-19 11:16:32 +0530

    Update lab.py

--- Dictionary/lab.py
+++ Dictionary/lab.py
@@ -1,88 +1,39 @@
 def create_dict_from_lists(keys, values):
-    """
-    Instead of returning 0, this method should
-    Create a dictionary from lists of keys and values.
-
-    Hint: Use dict() and zip()
-
-    :param keys: A list of keys for the dictionary.
-    :param values: A list of values for the dictionary.
-    :return: The created dictionary.
-    """
+    return dict(zip(keys,values))
+    return 0
     return 0
 
 
 def clear_dict(my_dict):
-    """
-    this method should Remove all elements from the dictionary.
-
-    :param my_dict: The dictionary to clear.
-    """
-    # write your code here
+    my_dict.clear()
 
 
 def copy_dict(my_dict):
-    """
-    Instead of returning 0, this method should Return a copy of the dictionary.
-
-    :param my_dict: The dictionary to copy.
-    :return: A copy of the dictionary.
-    """
+    return my_dict.copy()
     return 0
 
 
-
-
 def get_value(my_dict, key):
-    """
-    Instead of returning 0, this method should 
-    Return the value of the specified key.
-
-    :param my_dict: The dictionary.
-    :param key: The key to retrieve the value for.
-    :return: The value associated with the specified key, or None if the key is not found.
-    """
+    return my_dict.get(key)
     return 0
 
 
 def get_items(my_dict):
-    """
-    Instead of returning 0, this method should 
-    Return a list containing a tuple for each key-value pair in the dictionary.
-
-    :param my_dict: The dictionary.
-    :return: A list of tuples containing key-value pairs.
-    """
+    return list(my_dict.items())
     return 0
 
 
 def get_keys(my_dict):
-    """
-    Instead of returning 0, this method should Return a list containing the dictionary's keys.
-
-    :param my_dict: The dictionary.
-    :return: A list of keys.
-    """
+    return list(my_dict.keys())
     return 0
 
 
 def remove_key(my_dict, key):
-    """
-    Instead of returning 0, this method should Remove the element with the specified key.
-
-    :param my_dict: The dictionary.
-    :param key: The key to remove.
-    :return: The value associated with the removed key, or None if the key is not found.
-    """
+    return my_dict.pop(key,none)
     return 0
 
 
 
 def get_values(my_dict):
-    """
-    Instead of returning 0, this method should Return a list of all the values in the dictionary.
-
-    :param my_dict: The dictionary.
-    :return: A list of values.
-    """
+    return list(my_dict.values())
     return 0
